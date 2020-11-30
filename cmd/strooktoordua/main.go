d69071dc552419cfae77b2ea76626b8bf5428b43
Cokorda Raka <raka.moviz@gmail.com>
2020-11-30 03:18:56 -0600

    initial

---
+++ cmd/strooktoordua/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+  //"github.com/rakamoviz/strooktoorsatu/internal/internala"
+	"github.com/rakamoviz/strooktoorsatu/pkg/poobleeka"
+)
+
+func main() {
+	fmt.Println("strooktoorduamain")
+	strook := poobleeka.Strook{Name: "strook external"}
+  //streek := internala.Streek{Name: "streek external"}
+	fmt.Println(strook)
+  //fmt.Println(streek)
+}
+
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rakamoviz/strooktoordua
+
+go 1.15
+
+require github.com/rakamoviz/strooktoorsatu v0.0.0-20201130091159-7807e86796c3 // indirect
